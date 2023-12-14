7c649ba35284a962360bbda2bb00c68925664318
Yaroslav Ovdii <yaovdiy@gmail.com>
2023-12-14 23:08:49 +0200

    feat: added speedinsights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 	inject({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
 </script>
 
 <slot />
